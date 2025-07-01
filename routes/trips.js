220fc9983a3c8c227e3e3dcffe447c77b2f27958
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-01 10:59:00 +0200

    ajout route get trips depart arrivée timestamp

--- routes/trips.js
+++ routes/trips.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Trip = require('../models/trips');
+const {param, validationResult} = require('express-validator');
+const moment = require('moment');
 
 router.get('/', async (req, res) => {
     try {
@@ -15,5 +17,33 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+router.get('/:departure/:arrival/:timestamp',
+    param('departure').escape().isLength({max: 100}),
+    param('arrival').escape().isLength({max: 100}),
+    param('timestamp').escape().isLength({max: 100}),
+    async function (req, res, next) {
+    try {
+        const errors = validationResult(req);
+        if (errors.length > 0) {
+            return res.json({result: false, error: errors});
+        }
+        const date =new Date(req.params.timestamp * 1000)
+        const today = new Date(date.getTime());
+        today.setUTCHours(0,0,0,0);
+        const tomorrow = new Date(date.getTime());
+        tomorrow.setUTCHours(23,59,59,999);
+        console.log(date, today, tomorrow);
+        const tripList = await Trip.find({
+                    departure: req.params.departure,
+                    arrival: req.params.arrival,
+                    date: {$gte: today, $lte: tomorrow}
+                })
+        res.json({result: true, trips: tripList});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({result: false, error});
+    }
+})
 
 module.exports = router;

e39e1bed68ded68160933305df9eb3f3c5f8ce8c
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-02 11:48:25 +0200

    fekosp

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ var app = express();
 const cors = require('cors');
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
